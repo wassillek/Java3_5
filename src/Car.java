efd718f3389e41bd2464c61eee40714ed2a8ed6b
wassillek <wassillek@rambler.ru>
2021-04-29 18:30:43 +0300

    1

---
+++ src/Car.java
@@ -0,0 +1,63 @@
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+
+public class Car implements Runnable {
+    private static int CARS_COUNT;
+    static {
+        CARS_COUNT = 0;
+    }
+    private Race race;
+    private int speed;
+    private String name;
+
+    public int countStagesIsFinished = 0;
+    public static int countStagesToFinished = 0;
+
+    private static CyclicBarrier barrier = null;
+    public String getName() {
+        return name;
+    }
+    public int getSpeed() {
+        return speed;
+    }
+    public Car(Race race, int speed) {
+        this.race = race;
+        this.speed = speed;
+        CARS_COUNT++;
+        this.name = "Участник #" + CARS_COUNT;
+
+        if (this.barrier == null) {
+            this.barrier = new CyclicBarrier(Main.CARS_COUNT);
+        }
+    }
+    @Override
+    public void run() {
+        try {
+            System.out.println(this.name + " готовится");
+            Thread.sleep(500 + (int)(Math.random() * 800));
+            System.out.println(this.name + " готов");
+            barrier.await();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        try {
+            Main.readyToStart.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (BrokenBarrierException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            Main.start.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (BrokenBarrierException e) {
+            e.printStackTrace();
+        }
+
+        for (int i = 0; i < race.getStages().size(); i++) {
+            race.getStages().get(i).go(this);
+        }
+    }
+}
